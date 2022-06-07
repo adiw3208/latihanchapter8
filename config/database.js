df09c69ccab0fb1b5350b9b6cfe7491106c0a38b
ADI WAHYUDI <adiw3208@gmail.com>
2022-06-07 16:13:27 +0700

    update config lagi

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const {
   DB_USER = "postgres",
-  DB_PASSWORD = "3208",
+  DB_PASSWORD = "",
   DB_NAME = "bcr",
   DB_HOST = "127.0.0.1",
-  DB_PORT = "3208",
+  DB_PORT = "5432",
 } = process.env;
 
 module.exports = {
