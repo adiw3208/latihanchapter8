c7123723fe438a29ec25b1f14fc4c39dee53765b
ADI WAHYUDI <adiw3208@gmail.com>
2022-06-07 17:46:30 +0700

    make it better ngehehe

--- config/database.js
+++ config/database.js
@@ -1,11 +1,10 @@
 const {
-  DB_USER = "postgres",
+  DB_USER = "",
   DB_PASSWORD = "",
   DB_NAME = "bcr",
   DB_HOST = "127.0.0.1",
   DB_PORT = "5432",
 } = process.env;
-
 module.exports = {
   development: {
     username: DB_USER,
@@ -14,6 +13,12 @@ module.exports = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USER,
@@ -22,6 +27,12 @@ module.exports = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     username: DB_USER,
@@ -30,5 +41,11 @@ module.exports = {
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
